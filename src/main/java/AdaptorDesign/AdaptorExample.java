16bfff395de24f7544e2d3a15850d74c9203fdc2
Rahul <rahtiwari@gmail.com>
2021-05-30 19:43:05 +0530

    for PR example to Rohit

--- src/main/java/AdaptorDesign/AdaptorExample.java
+++ src/main/java/AdaptorDesign/AdaptorExample.java
@@ -17,6 +17,7 @@ public class AdaptorExample {
         AndoidtIphoneCharger andoidtIphoneCharger = new AndoidtIphoneCharger(Oneplus);
 
         andoidtIphoneCharger.iphonecharge();
+        System.out.println("For merge");
 
 
         }
